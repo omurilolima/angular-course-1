24afa22517d3f667bcad6d8656b107d1ee111b5b
Murilo Lima <murilo.serafim@clin.digital>
2026-03-13 18:50:27 +0000

    Component Inputs and the @Input Decorator

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from "@angular/core";
+import { COURSES } from "src/db-data";
+import { Course } from "../model/course";
 
 @Component({
-  selector: 'course-card',
+  selector: "course-card",
   imports: [],
-  templateUrl: './course-card.component.html',
-  styleUrl: './course-card.component.css',
+  templateUrl: "./course-card.component.html",
+  styleUrl: "./course-card.component.css",
 })
 export class CourseCardComponent {
-
+  @Input()
+  course: Course;
 }
